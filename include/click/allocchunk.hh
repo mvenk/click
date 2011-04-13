d2d6c03dd4073d78559923ccd3f41fdad1cb1d9d
Madhuri Venkatesh <madhureev@gmail.com>
2011-04-12 22:40:50 -0700

    Added radixiplookup14.cc which allocates the radix nodes using a custom allocator. The files for the custom allocator are in lib/allocchunk.cc and include/click/allocchunk.cc. These files have been altered to implement a singleton pattern, since we do not want to include a reference to the custom-allocator in each radix node.

--- include/click/allocchunk.hh
+++ include/click/allocchunk.hh
@@ -21,12 +21,18 @@ private:
   int multiple;
   struct block_list *head;
   struct block_list *tail;
-public:
-  AllocChunk(int size,int multiple);
+  int _is_created;
+  AllocChunk();
   ~AllocChunk();
+public:
+  static AllocChunk* Instance();
+  AllocChunk(AllocChunk const&){};             // copy constructor is private
+  AllocChunk& operator=(AllocChunk const&);  // assignment operator is private
+  static AllocChunk* m_pInstance;
   void init_block_list(void);
   void add_block(void);
   void * alloc(void);
   void free_all(void);
+  void create(int size,int multiple);
 };
 #endif
